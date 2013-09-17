4b247e983094aac1808bb3f8f3c552f1baea5833
Ngoc Nguyen <nguyenngoc2505@gmail.com>
2013-09-17 16:16:16 +0700

    edit

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,15 +1,21 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
+  # menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    # div :class => "blank_slate_container", :id => "dashboard_default_message" do
+    #   span :class => "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
+    section "Image", :priority => 3 do
+      div do
+        br
+        text_node %{<img src="http://framgia.com/images/banner_top.png" frameborder="no"></img>}.html_safe
       end
     end
-    section "Recent Products" do
+    section "Recent Products", :priority => 1 do
       table_for Product.order("name desc").limit(5) do
         column :name do |product|
           link_to product.name, [:admin, product]
@@ -18,6 +24,7 @@ ActiveAdmin.register_page "Dashboard" do
       end
       strong { link_to "View All Products", admin_products_path }
     end
+    
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
